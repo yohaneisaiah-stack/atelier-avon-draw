ba9ccb9d75bcbf943a78010d5cc36a3ba2049981
yohaneisaiah-stack <yohane.isaiah@gmail.com>
2026-08-16 22:52:59 +0900

    Refresh PWA cache for V0.2

--- sw.js
+++ sw.js
@@ -1,3 +1,5 @@
-const CACHE='avon-draw-v01'; const FILES=['./','./index.html','./manifest.webmanifest'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES))));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const CACHE='avon-draw-v02';
+const FILES=['./','./index.html','./manifest.webmanifest'];
+self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)))});
+self.addEventListener('activate',e=>e.waitUntil(Promise.all([caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))),self.clients.claim()])));
+self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request))));
